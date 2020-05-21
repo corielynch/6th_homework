d06d0142fa4dbb098635059bdc5d9a904facce4b
Corie Lynch <Lynchc17@gmail.com>
2020-05-20 18:46:26 -0700

    Functionality for ajax

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 $(document).ready(function () {
-  $("#button-addon1").click(function(){
+  $(".btn").click(function(){
     var city = $(".form-control").val();
-    // if (city != ""){
-      
+
+    var APIKey = "438d21396eadb2777d54f41c8be79e21";
+
+    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+    
+      .then(function(response) {
+        console.log(response);
+
+      });
     });
 });
   
 
 //This is the API key
-var APIKey = "438d21396eadb2777d54f41c8be79e21";
 
-var queryURL = "api.openweathermap.org/data/2.5/forecast?q=" + city + "&units=imperial&appid=" + APIKey;
-
-$.ajax({
-    url: queryURL,
-    method: "GET"
-  })
-
-  .then(function(response) {
-    console.log(queryURL);
-
-  });
 
 
   //Saves input info to local storage
